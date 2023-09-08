54b8d96be3fc8021b89341d30b9bc6c4e69c8aa7
Oromion Aznarán <caznaranl@uni.pe>
2023-09-08 08:40:16 -0500

    Update

--- p7.py
+++ p7.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Interpolacion de Lagrange
 # Polinomio en forma simbólica
 import numpy as np
@@ -7,31 +9,33 @@ import matplotlib.pyplot as plt
 plt.rcParams["text.usetex"] = True
 plt.rcParams["font.serif"] = ["Computer Modern"]
 
-print('INTERPOLACIÓN POLINÓMICA DE LAGRANGE', end="\n\n")
-
-print("Es una forma de presentar el polinomio que interpola un conjunto de puntos dado. ", end="\n\n")
+print("INTERPOLACIÓN POLINÓMICA DE LAGRANGE", end="\n\n")
+print(
+    "Es una forma de presentar el polinomio que interpola un conjunto de puntos dado. ",
+    end="\n\n",
+)
 
 # INGRESO , Datos de prueba
 
 # Datos de X
-xi = np.array([0, 0.5, 1, 2])
+xi = np.array([0, 1/2, 1, 2])
 
 # Datos de F(X)
-fi = np.array([1, 2, 1.5, -1])
+fi = np.array([1, 2, 3/2, -1])
 
 # PROCEDIMIENTO
 n = len(xi)
-x = sym.Symbol('x')
+x = sym.Symbol("x")
 # Polinomio
 polinomio = 0
 for i in range(0, n, 1):
     # Termino de Lagrange
     termino = 1
     for j in range(0, n, 1):
-        if (j != i):
-            termino = termino*(x-xi[j])/(xi[i]-xi[j])
+        if j != i:
+            termino = termino * (x - xi[j]) / (xi[i] - xi[j])
     print("El L(", i, ") es :", termino)
-    polinomio = polinomio + termino*fi[i]
+    polinomio = polinomio + termino * fi[i]
 # Expande el polinomio
 px = polinomio.expand()
 # para evaluacion numérica
@@ -45,15 +49,15 @@ xi_p = np.linspace(a, b, muestras)
 fi_p = pxn(xi_p)
 
 # Salida
-print('\nPolinomio de Lagrange, expresiones:')
+print("\nPolinomio de Lagrange, expresiones:")
 print(polinomio)
 print()
-print('Polinomio de Lagrange: ')
+print("Polinomio de Lagrange: ")
 print(px)
 
 # Gráfica
-plt.title('Interpolación Lagrange')
-plt.plot(xi, fi, 'o', label='Puntos')
-plt.plot(xi_p, fi_p, label='Polinomio')
+plt.title("Interpolación Lagrange")
+plt.plot(xi, fi, "o", label="Puntos")
+plt.plot(xi_p, fi_p, label="Polinomio")
 plt.legend()
-plt.savefig("p7.pdf")
+plt.savefig("p7.pdf", transparent=True, bbox_inches="tight")
