452a285aea662a71415878f673a9690b53c13bc9
Oromion Aznarán <caznaranl@uni.pe>
2023-09-07 23:38:07 -0500

    Update

--- p6.py
+++ p6.py
@@ -11,14 +11,26 @@ plt.rcParams["font.serif"] = ["Computer Modern"]
 x = np.array([-1, 0, 1])
 y = np.array([1/2, 1, -1])
 poly = lagrange(x, y)
-print(poly)
-
+# print(poly)
 p = Polynomial(poly.coef[::-1]).coef
-print(p)
+# print(p)
 delta = 0.1
-x_new = np.arange(start=-1 - delta, stop=1 + delta, step=0.1)
+x_new = np.arange(start=-1 - delta, stop=1 + delta, step=0.01)
 
-plt.scatter(x, y, label='data')
-plt.plot(x_new, Polynomial(poly.coef[::-1])(x_new), label='Polynomial')
+
+plt.scatter(x, y, s=10, label='Puntos de control')
+plt.plot(x_new, Polynomial(
+    poly.coef[::-1])(x_new), 'orange', lw=0.75, label=f"${{{p[2]}}}x^2{{{p[1]}}}x+{{{p[0]}}}$")
+plt.grid()
 plt.legend()
+plt.title(f"Polinomio de interpolación de Lagrange")
 plt.savefig("p6.pdf")
+
+plt.cla()
+plt.plot(x_new, 0.5*x_new*(x_new-1), lw=0.75, label=f"$\ell_{0}(t)$")
+plt.plot(x_new, -(x_new+1)*(x_new-1), lw=0.75, label=f"$\ell_{1}(t)$")
+plt.plot(x_new, 0.5*(x_new+1)*x_new, lw=0.75, label=f"$\ell_{2}(t)$")
+plt.grid()
+plt.legend()
+plt.title(f"Polinomios de Lagrange")
+plt.savefig("p6_lagrange.pdf")
