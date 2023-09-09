719f0b955408a51db4283907a91d2784e9821c85
Oromion Aznarán <caznaranl@uni.pe>
2023-09-09 09:34:30 -0500

    Update

--- p1.py
+++ p1.py
@@ -22,19 +22,19 @@ def bernstein(t: np.array, k: int, n: int):
 
 k = 3
 n = 5
-t = np.linspace(start=0, stop=1, num=100)
-kappa = (200 * t**3 - 240 * t**2 + 60 * t) / (
+t = np.linspace(start=0, stop=1, num=300)
+kappa = np.abs(200 * t**3 - 240 * t**2 + 60 * t) / (
     1 + (50 * t**4 - 80 * t**3 + 30 * t**2) ** 2
 ) ** (3 / 2)
 
-plt.plot(t, bernstein(t=t, k=k, n=n), lw=0.75, label=f"$10t^5-20t^4+10t^3$")
+plt.plot(t, bernstein(t=t, k=k, n=n), lw=0.45, label=f"$10t^5-20t^4+10t^3$")
 plt.grid()
 plt.legend()
 plt.title(f"Polinomio de Bernstein $B_{{{k},{n}}}(t)$")
 plt.savefig("p1.pdf", transparent=True, bbox_inches="tight")
 plt.cla()
 
-plt.plot(t, kappa, lw=0.75, label=f"${{\kappa}}(t)$")
+plt.plot(t, kappa, lw=0.45, label=f"${{\kappa}}(t)$")
 plt.grid()
 plt.legend()
 plt.title(f"Función curvatura ${{\kappa}}(t)$ de $B_{{{k},{n}}}(t)$")
--- p12.py
+++ p12.py
@@ -24,9 +24,9 @@ def plot_lagrange(numbers):
         delta = 0.1
         x_new = np.arange(start=-5 - delta, stop=5 + delta, step=0.01)
         plt.plot(x_new, Polynomial(poly.coef[::-1])(x_new), 'orange',
-                 lw=0.75, label=f"$P_{{{number}}}(t)$")
+                 lw=0.45, label=f"$P_{{{number}}}(t)$")
         plt.scatter(x, y, s=10, label='Puntos de control')
-        plt.plot(t, y_new, lw=0.7, label=r"$f(t)=\frac{1}{1+t^2}$")
+        plt.plot(t, y_new, lw=0.45, label=r"$f(t)=\frac{1}{1+t^2}$")
         # plt.plot(x, y)
         plt.grid()
         plt.legend()
@@ -39,7 +39,7 @@ def plot():
     delta = 0.5
     t = np.linspace(start=-5 - delta, stop=5 + delta, num=300)
     y = 1 / (1 + t**2)
-    plt.plot(t, y, lw=0.7, label=r"$f(t)=\frac{1}{1+t^2}$")
+    plt.plot(t, y, lw=0.45, label=r"$f(t)=\frac{1}{1+t^2}$")
     plt.grid()
     plt.legend()
     plt.title("Función de Carl Runge")
