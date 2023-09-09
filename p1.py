43b793660000fdef987742a501a2eb5072932538
Oromion Aznarán <caznaranl@uni.pe>
2023-09-09 10:58:05 -0500

    Update

--- p1.py
+++ p1.py
@@ -20,22 +20,30 @@ def bernstein(t: np.array, k: int, n: int):
     return comb(n, k) * t**k * (1 - t) ** (n - k)
 
 
-k = 3
-n = 5
-t = np.linspace(start=0, stop=1, num=300)
-kappa = np.abs(200 * t**3 - 240 * t**2 + 60 * t) / (
-    1 + (50 * t**4 - 80 * t**3 + 30 * t**2) ** 2
-) ** (3 / 2)
+def plot_bernstein(k: int = 3, n: int = 5):
+    t = np.linspace(start=0, stop=1, num=100)
+    plt.cla()
+    plt.plot(t, bernstein(t=t, k=k, n=n), lw=0.45,
+             label=f"$10t^5-20t^4+10t^3$")
+    plt.grid()
+    plt.legend(title="Leyenda", shadow=True, fancybox=True)
+    plt.title(f"Polinomio de Bernstein $B_{{{k},{n}}}(t)$")
+    plt.savefig("p1.pdf", transparent=True, bbox_inches="tight")
 
-plt.plot(t, bernstein(t=t, k=k, n=n), lw=0.45, label=f"$10t^5-20t^4+10t^3$")
-plt.grid()
-plt.legend()
-plt.title(f"Polinomio de Bernstein $B_{{{k},{n}}}(t)$")
-plt.savefig("p1.pdf", transparent=True, bbox_inches="tight")
-plt.cla()
 
-plt.plot(t, kappa, lw=0.45, label=f"${{\kappa}}(t)$")
-plt.grid()
-plt.legend()
-plt.title(f"Función curvatura ${{\kappa}}(t)$ de $B_{{{k},{n}}}(t)$")
-plt.savefig("p1_curvature.pdf", transparent=True, bbox_inches="tight")
+def plot_curvature():
+    t = np.linspace(start=0, stop=1, num=500)
+    kappa = np.abs(200 * t**3 - 240 * t**2 + 60 * t) / (
+        1 + (50 * t**4 - 80 * t**3 + 30 * t**2) ** 2
+    ) ** (3 / 2)
+    plt.cla()
+    plt.plot(t, kappa, lw=0.45, label=f"${{\kappa}}(t)$")
+    plt.grid()
+    plt.legend(title="Leyenda", shadow=True, fancybox=True)
+    plt.title(f"Función curvatura ${{\kappa}}(t)$ de $B_{{3, 5}}(t)$")
+    plt.savefig("p1_curvature.pdf", transparent=True, bbox_inches="tight")
+
+
+if __name__ == "__main__":
+    plot_bernstein()
+    plot_curvature()
