9d509eb7bf1718259fcc76fc94d4a8d307f81455
Oromion Aznarán <caznaranl@uni.pe>
2023-09-09 13:50:37 -0500

    Update

--- p12.py
+++ p12.py
@@ -8,7 +8,7 @@ from numpy.polynomial.polynomial import Polynomial
 plt.rcParams["text.usetex"] = True
 plt.rcParams["font.serif"] = ["Computer Modern"]
 
-numbers = [5, 8, 10]
+numbers = (5, 8, 10)
 
 
 def plot_lagrange(numbers):
@@ -24,14 +24,15 @@ def plot_lagrange(numbers):
         delta = 0.1
         x_new = np.arange(start=-5 - delta, stop=5 + delta, step=0.01)
         plt.plot(x_new, Polynomial(poly.coef[::-1])(x_new), 'orange',
-                 lw=0.45, label=f"$P_{{{number}}}(t)$")
+                 lw=0.45, label=f"$P_{{{number-1}}}(t)$")
         plt.scatter(x, y, s=10, label='Puntos de control')
         plt.plot(t, y_new, lw=0.45, label=r"$f(t)=\frac{1}{1+t^2}$")
         # plt.plot(x, y)
         plt.grid()
-        plt.legend()
+        plt.legend(title="Leyenda", shadow=True, fancybox=True)
         plt.title("Polinomio de Lagrange")
-        plt.savefig(f"p12_lagrange{number}.pdf")
+        plt.savefig(f"p12_lagrange{number}.pdf",
+                    transparent=True, bbox_inches="tight")
         plt.cla()
 
 
@@ -41,10 +42,11 @@ def plot():
     y = 1 / (1 + t**2)
     plt.plot(t, y, lw=0.45, label=r"$f(t)=\frac{1}{1+t^2}$")
     plt.grid()
-    plt.legend()
+    plt.legend(title="Leyenda", shadow=True, fancybox=True)
     plt.title("Función de Carl Runge")
-    plt.savefig("p12.pdf")
+    plt.savefig("p12.pdf", transparent=True, bbox_inches="tight")
 
 
-plot_lagrange(numbers=numbers)
-# plot()
+if __name__ == "__main__":
+    plot_lagrange(numbers=numbers)
+    plot()
