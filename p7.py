2a238babf71b1caa8f603216580c659816d26992
Oromion Aznarán <caznaranl@uni.pe>
2023-09-09 13:39:18 -0500

    Update

--- p7.py
+++ p7.py
@@ -28,13 +28,13 @@ def plot_interpolation(t: np.array, x: np.array):
     # print(p)
     plt.scatter(t, x, s=10, label="Puntos de control")
     delta = 0.1
-    t_new = np.arange(start=-1 - delta, stop=1 + delta, step=0.01)
+    t_new = np.arange(start=0 - delta, stop=2 + delta, step=0.01)
     plt.plot(
         t_new,
         Polynomial(poly.coef[::-1])(t_new),
         "orange",
         lw=0.45,
-        label=f"$p_3(t)={{{p[3]}}}t^3{{{p[2]}}}t^2{{{p[1]}}}t+{{{p[0]}}}$",
+        label=f"$p_3(t)={{{np.round(p[3])}}}t^3{{{p[2]}}}t^2+{{{np.round(p[1])}}}t+{{{p[0]}}}$",
     )
     plt.grid()
     plt.legend(title="Leyenda", shadow=True, fancybox=True)
@@ -45,23 +45,23 @@ def plot_interpolation(t: np.array, x: np.array):
 def plot_basis_lagrange(t: np.array, x: np.array):
     plt.cla()
     delta = 0.1
-    t_new = np.arange(start=-1 - delta, stop=1 + delta, step=0.01)
+    t_new = np.arange(start=0 - delta, stop=2 + delta, step=0.01)
     plt.scatter(t, np.zeros_like(t), s=10)
     plt.plot(
         t_new,
-        1 / 2 * t_new**2 - 1 / 2 * t_new,
+        -(t_new**3) + 7 / 2 * t_new**2 - 7 / 2 * t_new + 1,
         lw=0.45,
         label=r"$\ell_{0}(t)=-t^3+\frac{7}{2}t^2-\frac{7}{2}t+1$",
     )
     plt.plot(
         t_new,
-        1 - t_new**2,
+        8 / 3 * t_new**3 - 8 * t_new**2 + 16 / 3 * t_new,
         lw=0.45,
         label=r"$\ell_{1}(t)=\frac{8}{3}t^3-8t^2+\frac{16}{3}t$",
     )
     plt.plot(
         t_new,
-        1 / 2 * t_new**2 + t_new / 2,
+        -2 * t_new**3 + 5 * t_new**2 - 2 * t_new,
         lw=0.45,
         label=r"$\ell_{2}(t)=-2t^3+5t^2-2t$",
     )
